796f15aea3385340ad4909bed8f158e233a20296
AYMEN <moniakamli95@gmail.com>
2021-04-29 12:51:31 +0200

    add showAddTask state and add button toggle

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import AddTask from "./components/AddTask";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     { 
         id: 1,
@@ -45,8 +46,12 @@ function App() {
 
   return (
     <div className="container">
-      <Header title="Task Tracker" />
-      <AddTask onAdd={addTask}/>
+      <Header 
+        title="Task Tracker" 
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      { showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> :
        'No tasks to show. '}
     </div>
